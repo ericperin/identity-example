54b38f913a444998bc8b481b3c3edb845608e8fe
Eric Perin <eric.perin@outlook.com>
2019-10-29 20:14:54 -0400

    Added client credentials flow

--- src/TCE.Identity/Config.cs
+++ src/TCE.Identity/Config.cs
@@ -13,11 +13,25 @@ namespace TCE.Identity
 
         public static IEnumerable<ApiResource> Apis =>
             new ApiResource[]
-            { };
+            {
+                new ApiResource("api_tce", "API do TCE-RO")
+            };
 
         public static IEnumerable<Client> Clients =>
             new Client[]
-            { };
+            {
+                // client credentials flow client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientName = "Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedScopes = { "api_tce" }
+                }, 
+            };
 
     }
 }
